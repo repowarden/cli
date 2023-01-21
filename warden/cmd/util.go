6f5a7df86cd374fd27a1ffaaed38401e79d0c14d
Ricardo N Feliciano <Ricardo@Feliciano.Tech>
2023-01-21 17:03:51 -0500

    Fix the use of file flags

--- warden/cmd/util.go
+++ warden/cmd/util.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/fs"
 	"os"
+	"strings"
 
 	"gopkg.in/yaml.v3"
 )
@@ -15,32 +16,17 @@ import (
 func loadPolicyFile(customPath string) (*PolicyFile, []byte, error) {
 
 	var file PolicyFile
-	var foundFile bool
 	var yamlContent []byte
 	var err error
 
-	possibleFilepaths := []string{"policy."}
-
-	if customPath != "" {
-		possibleFilepaths = append([]string{customPath}, possibleFilepaths...)
-	}
-
-	for _, path := range possibleFilepaths {
-
-		yamlContent, err = loadYAMLFile(path)
-		if err == nil {
-			foundFile = true
-			break
-		}
+	yamlContent, err = loadYAMLFile("policy.yml", customPath)
+	if err != nil {
+		return nil, nil, err
 	}
 
 	err = yaml.Unmarshal(yamlContent, &file)
 	if err != nil {
 		return nil, nil, err
-	}
-
-	if !foundFile {
-		return nil, nil, fmt.Errorf("A policy file was not found. Either './policy.yml' needs to be used or the '--policyFile' flag set.")
 	}
 
 	return &file, yamlContent, nil
@@ -53,47 +39,50 @@ func loadRepositoriesFile(customPath string) ([]RepositoryDefinition, []byte, er
 
 	var repositoriesFile []RepositoryDefinition
 	var yamlContent []byte
-	var foundFile bool
 	var err error
 
-	possibleFilepaths := []string{"repositories."}
-
-	if customPath != "" {
-		possibleFilepaths = append([]string{customPath}, possibleFilepaths...)
-	}
-
-	for _, path := range possibleFilepaths {
-
-		yamlContent, err = loadYAMLFile(path)
-		if err == nil {
-			foundFile = true
-			break
-		}
+	yamlContent, err = loadYAMLFile("repositories.yml", customPath)
+	if err != nil {
+		return nil, nil, fmt.Errorf("A Repositories file was not found. Either './repositories.yml' needs to be used or the '--repositoriesFile' flag set.")
 	}
 
 	err = yaml.Unmarshal(yamlContent, &repositoriesFile)
 	if err != nil {
-		return nil, nil, err
-	}
-
-	if !foundFile {
-		return nil, nil, fmt.Errorf("A Repositories file was not found. Either './repositories.yml' needs to be used or the '--repositoriesFile' flag set.")
+		return nil, nil, fmt.Errorf("The repositories file couldn't be parsed. Something is wrong.")
 	}
 
 	return repositoriesFile, yamlContent, nil
 }
 
-// loadYAMLFile loads and unmarshals a YAML file. Both .yml and .yaml will be
-// attempted and in that order, so end the filename with a period. For example:
-// `my-file.` or `repositories.`.
-func loadYAMLFile(filepath string) ([]byte, error) {
+// loadYAMLFile attempts to load a YAML file based on one or more possible file
+// names. Both .yml and .yaml will be attempted and in that order.
+func loadYAMLFile(filepaths ...string) ([]byte, error) {
 
 	var yamlContent []byte
+	var possiblePaths []string
 	var err error
 
-	possibleFiles := []string{filepath + "yml", filepath + "yaml"}
+	if len(filepaths) == 0 {
+		return nil, fmt.Errorf("At least one filepath needs to be provided.")
+	}
 
-	for _, path := range possibleFiles {
+	for _, path := range filepaths {
+
+		if strings.HasSuffix(path, ".yml") {
+			path = path[0 : len(path)-3]
+		} else if strings.HasSuffix(path, ".yaml") {
+			path = path[0 : len(path)-4]
+		} else if path == "" {
+			continue
+		} else {
+			return nil, fmt.Errorf("Only YAML files are supported.")
+		}
+
+		possiblePaths = append(possiblePaths, path+"yml")
+		possiblePaths = append(possiblePaths, path+"yaml")
+	}
+
+	for _, path := range filepaths {
 
 		yamlContent, err = os.ReadFile(path)
 		if errors.Is(err, fs.ErrNotExist) {
