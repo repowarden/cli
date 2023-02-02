831e4ec213a2fa7e93cca46bb7f71832f58c9bf8
Ricardo N Feliciano <Ricardo@Feliciano.Tech>
2023-02-02 02:16:47 -0500

    Improve policy error text and better organize

---
+++ warden/cmd/errors-policy.go
@@ -0,0 +1,24 @@
+package cmd
+
+import "fmt"
+
+const (
+	ERR_ACCESS_MISSING = "The repository %s doesn't have the user."
+	ERR_ACCESS_WRONG   = "The repository %s's user's permission is incorrect."
+	ERR_BRANCH_DEFAULT = "The default branch should be '%s', not '%s'."
+	ERR_LABEL_EXTRA    = "The label '%s' is present and shouldn't be."
+	ERR_LABEL_MISSING  = "The label '%s' is missing."
+	ERR_LICENSE        = "The license should be '%s', not '%s'."
+)
+
+// Represents a error when applying a policy, with contextual information.
+type PolicyError struct {
+	repository RepositoryDefinition
+	message    string
+	values     []any
+}
+
+// Properly print out a policy error.
+func (this PolicyError) Error() string {
+	return fmt.Sprintf(this.message, this.values...)
+}
