04cc1babae97bf62a37ddc94ea99c6bbc53c59ff
Ricardo N Feliciano <Ricardo@Feliciano.Tech>
2023-02-02 15:53:27 -0500

    Support auditing of CODEOWNERS files

--- warden/cmd/errors-policy.go
+++ warden/cmd/errors-policy.go
@@ -9,6 +9,9 @@ const (
 	ERR_LABEL_EXTRA    = "The label '%s' is present and shouldn't be."
 	ERR_LABEL_MISSING  = "The label '%s' is missing."
 	ERR_LICENSE        = "The license should be '%s', not '%s'."
+	ERR_CO_DIFFERENT   = "The CODEOWNERS file is different from the policy."
+	ERR_CO_MISSING     = "The CODEOWNERS file is not present."
+	ERR_CO_SYNTAX      = "The CODEOWNERS file has syntax errors:\n%s"
 )
 
 // Represents a error when applying a policy, with contextual information.
