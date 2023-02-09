2f40569d8d046fcc03f3abb66705743fbea8f558
Ricardo N Feliciano <Ricardo@Feliciano.Tech>
2023-02-09 01:11:15 -0500

    Filter codeownersPolicy by tag

--- warden/cmd/policy-file.go
+++ warden/cmd/policy-file.go
@@ -6,13 +6,13 @@ package cmd
 
 // The top-level structure representing a policy.yml file.
 type PolicyFile struct {
-	DefaultBranch string         `yaml:"defaultBranch"`
-	Archived      bool           `yaml:"archived"` // include archived repos in lookup?
-	License       *licensePolicy `yaml:"license"`
-	Labels        []string       `yaml:"labels"`
-	LabelStrategy string         `yaml:"labelStrategy"`
-	Access        []accessPolicy `yaml:"access"`
-	CodeOwners    string         `yaml:"codeowners"`
+	DefaultBranch string             `yaml:"defaultBranch"`
+	Archived      bool               `yaml:"archived"` // include archived repos in lookup?
+	License       *licensePolicy     `yaml:"license"`
+	Labels        []string           `yaml:"labels"`
+	LabelStrategy string             `yaml:"labelStrategy"`
+	Access        []accessPolicy     `yaml:"access"`
+	Codeowners    []codeownersPolicy `yaml:"codeowners"`
 }
 
 // Which code licenses to allow and for which scope
